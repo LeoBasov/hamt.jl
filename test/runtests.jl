dfe91bda2e03acc8083e16ce4c9954ccbc93e1eb
Leo Basov <leo.basov@dlr.de>
2024-09-13 11:18:01 +0200

    implemented gmsh file reader

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using HAMT
 using Test
 
-@testset "HAMT.jl" begin
-	@test 1 == 1
+function test_read_Gmsh_file()
+	gmsh_file = HAMT.read_Gmsh_file("test_data/block_single_triangular.msh")
+
+	@test length(gmsh_file) == 4
+	@test length(gmsh_file["Nodes"]) == 6
+	@test gmsh_file["Nodes"][1][1] == "5"
+	@test length(gmsh_file["Nodes"][2]) == 4
+	@test gmsh_file["Nodes"][2][3] == "0.5"
+end
+
+@testset "Mesh.jl" begin
+	test_read_Gmsh_file()
 end
