76641f5d3c7528fa11a0b2aaeca918e7c32f7875
Leo Basov <leo.basov@dlr.de>
2024-09-17 11:08:53 +0200

    started implementation of mesh setting in solver

--- src/Solver.jl
+++ src/Solver.jl
@@ -11,5 +11,22 @@ function convert_triangular_mesh(mesh)
     matrix::Matrix{Float64} = zeros(length(mesh.nodes), length(mesh.nodes))
     vector::Vector{Float64} = zeros(length(mesh.nodes))
 
+    for node_id=1:length(mesh.nodes)
+        if length(mesh.nodes[node_id].boundaries) > 0
+            conver_boundaries!(matrix, vector, mesh, node_id)
+        else
+            convert_center!(matrix, vector, mesh, node_id)
+        end
+    end
+
     return matrix, vector
+end
+
+function convert_center!(matrix, vector, mesh, node_id)
+end
+
+function conver_boundaries!(matrix, vector, mesh, node_id)
+    #TODO
+    matrix[node_id, node_id] = 1.0
+    vector[node_id] = 1.0
 end
