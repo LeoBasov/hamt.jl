722d7de1a511f6384edaa3ebd1e2f9280b4c5d69
Leo Basov <leo.basov@dlr.de>
2024-09-17 10:52:50 +0200

    extended solver for solution

--- src/Solver.jl
+++ src/Solver.jl
@@ -1,5 +1,10 @@
+using LinearSolve
+
 function solve_heat_equation(mesh)
     matrix, vector = convert_triangular_mesh(mesh)
+    problem = LinearProblem(matrix, vector)
+    sol = solve(problem)
+    return sol.u
 end
 
 function convert_triangular_mesh(mesh)
