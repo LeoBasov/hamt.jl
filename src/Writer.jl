07f49349c6795763014874f0d77bff5eeb79cdc8
Leo Basov <leo.basov@dlr.de>
2024-09-17 13:03:37 +0200

    fixed writer

--- src/Writer.jl
+++ src/Writer.jl
@@ -4,6 +4,7 @@ using HAMT
 function write_mesh(mesh, solution)
     points::Matrix{Float64} = zeros(3, 3*length(mesh.cells))
     cells::Vector{MeshCell} = []
+    sol::Matrix{Float64} = zeros(1, 3*length(mesh.cells))
 
     for cell in mesh.cells
         push!(cells, MeshCell(VTKCellTypes.VTK_TRIANGLE, cell.nodes))
@@ -11,10 +12,11 @@ function write_mesh(mesh, solution)
             points[1, node_id] = mesh.nodes[node_id].position[1]
             points[2, node_id] = mesh.nodes[node_id].position[2]
             points[3, node_id] = mesh.nodes[node_id].position[3]
+            sol[1, node_id] = solution[node_id]
         end
     end
 
     vtk_grid("filename", points, cells) do vtk
-        vtk["temperature"] = solution
+        vtk["temperature", VTKPointData()] = sol
     end
 end
