2e34763dde2155654c79f0027f7c4078d5d44603
Leo Basov <leo.basov@dlr.de>
2024-09-17 16:00:09 +0200

    first implementation of neumann boundary

--- src/Solver.jl
+++ src/Solver.jl
@@ -1,4 +1,5 @@
 using LinearSolve
+using LinearAlgebra
 
 function solve_heat_equation(mesh)
     matrix, vector = convert_triangular_mesh(mesh)
@@ -76,7 +77,50 @@ function conver_boundaries!(matrix, vector, mesh, node_id)
     elseif boundary2.type == DIRICHLET
         matrix[node_id, node_id] = 1.0
         vector[node_id] = boundary2.value
+    elseif boundary1.type == NEUMANN && boundary2.type == NEUMANN
+        set_neumann_boundary!(matrix, vector, mesh, node_id)
     else
         error("undefined boundary type")
     end
+end
+
+function set_neumann_boundary!(matrix, vector, mesh, node_id)
+    node = mesh.nodes[node_id]
+    boundary1 = mesh.boundaries[node.boundaries[1]]
+    boundary2 = mesh.boundaries[node.boundaries[2]]
+    node_pos_left = mesh.nodes[node.adjacent_nodes[begin]].position
+    node_pos_right = mesh.nodes[node.adjacent_nodes[end]].position
+    rot_mat::Matrix{Float64} = zeros(3, 3)
+    total_cell_area = 0.0
+
+    rot_mat[1, 2] = 1.0;
+    rot_mat[2, 1] = -1.0;
+    rot_mat[3, 3] = 1.0;
+
+    normal = 0.5 * rot_mat * (node_pos_left - node.position) + 0.5 * rot_mat * (node.position - node_pos_right)
+    normal ./= norm(normal)
+
+    for cell_id in node.adjacent_cells
+        total_cell_area += get_cell_area(mesh, cell_id)
+    end
+
+    for cell_id in node.adjacent_cells
+        cell = mesh.cells[cell_id]
+        factor = 1.0 / (2.0 * total_cell_area)
+
+        for i=1:3
+            pos_im = i == 1 ? 3 : i - 1
+            pos_ip = i == 3 ? 1 : i + 1
+            node_id_im = cell.nodes[pos_im]
+            node_id_ip = cell.nodes[pos_ip]
+            node_id_i = cell.nodes[i]
+            node_pos_im = mesh.nodes[node_id_im].position
+            node_pos_ip = mesh.nodes[node_id_ip].position
+
+            matrix[node_id, node_id_i] += factor * dot(rot_mat * (node_pos_ip - node_pos_im), normal)
+        end
+    end
+
+    #TODO (LB): implement correct area dependant average
+    vector[node_id] = (boundary1.value + boundary2.value) / 2.0
 end
