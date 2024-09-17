8d71c9c2eadca633b10820f8d76334c39a6dad77
Leo Basov <leo.basov@dlr.de>
2024-09-17 10:52:56 +0200

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -110,3 +110,14 @@ end
 	test_convert_Gmsh2_to_Mesh()
 	test_ntr_mesh()
 end
+
+@testset "Solver.jl" begin
+	gmsh_file = HAMT.read_Gmsh_file("test_data/block_single_triangular.msh")
+	mesh = HAMT.convert_Gmsh2_to_Mesh(gmsh_file)
+	matrix, vector = HAMT.convert_triangular_mesh(mesh)
+	solution = HAMT.solve_heat_equation(mesh)
+
+	@test size(matrix) === (length(mesh.nodes), length(mesh.nodes))
+	@test length(vector) == length(mesh.nodes)
+	@test length(solution) == length(mesh.nodes)
+end
