14dd7ae38fd57cb77b729b82b89d822c5dc4767f
Leo Basov <leo.basov@dlr.de>
2024-09-27 14:50:38 +0200

    update ntr test case

--- examples/ntr/ntr.jl
+++ examples/ntr/ntr.jl
@@ -1,19 +1,18 @@
 using HAMT
 
-read_mesh("test/test_data/ntr.msh")
+read_mesh("examples/ntr/ntr.msh")
 
 set_boundary("attachment", DIRICHLET, 300.0)
-set_boundary("housing_top", DIRICHLET, 300.0)
-set_boundary("housing_buttom", DIRICHLET, 300.0)
+set_boundary("emitter_left", DIRICHLET, 1000.0)
+set_boundary("emitter_buttom", DIRICHLET, 1000.0)
 
-set_boundary("emitter_left", DIRICHLET, 500.0)
-set_boundary("emitter_buttom", DIRICHLET, 700.0)
-set_boundary("keeper_top", DIRICHLET, 300.0)
+set_boundary("keeper_top", RADIATION, 1.0)
+set_boundary("housing_top", RADIATION, 1.0)
+set_boundary("housing_buttom", RADIATION, 1.0)
+set_boundary("keeper_front", RADIATION, 1.0)
+set_boundary("keeper_buttom", RADIATION, 1.0)
+set_boundary("keeper_left", RADIATION, 1.0)
 
-set_boundary("keeper_front", DIRICHLET, 300.0)
-set_boundary("keeper_buttom", DIRICHLET, 300.0)
-set_boundary("keeper_left", DIRICHLET, 300.0)
-
-execute()
+execute(CYLINDER)
 
 export_solution("solution")
