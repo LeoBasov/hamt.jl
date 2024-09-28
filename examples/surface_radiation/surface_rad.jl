33e4c16d4c0e9bde3d9346b368a4860a9508b814
Leo Basov <leo.basov@dlr.de>
2024-09-28 11:19:48 +0200

    updated test case

--- examples/surface_radiation/surface_rad.jl
+++ examples/surface_radiation/surface_rad.jl
@@ -1,3 +1,14 @@
 using HAMT
 
 read_mesh("examples/surface_radiation/object.msh")
+
+set_boundary("left", DIRICHLET, 1000.0)
+set_boundary("top", RADIATION, 1.0)
+set_boundary("right", RADIATION, 1.0)
+set_boundary("buttom", RADIATION, 1.0)
+set_boundary("angle_up", RADIATION, 1.0)
+set_boundary("angle_down", RADIATION, 1.0)
+
+execute()
+
+export_solution("surface_rad")
