4bbc251266fdd7afb8cd4e15ad670b348a43eb8e
Leo Basov <leo.basov@dlr.de>
2024-10-21 15:24:55 +0200

    updated test case

--- examples/half_circle/half_circle.jl
+++ examples/half_circle/half_circle.jl
@@ -2,12 +2,14 @@ using HAMT
 
 read_mesh("examples/half_circle/circle_very_fine.msh")
 
-set_boundary("inner", DIRICHLET, 100.0)
-set_boundary("outer", DIRICHLET, 300.0)
+set_boundary("inner", NEUMANN, 0.0)
+set_boundary("outer", NEUMANN, 0.0)
 
-set_boundary("mirrow_left", NEUMANN, 0.0)
-set_boundary("mirrow_right", NEUMANN, 0.0)
+set_boundary("mirrow_left", DIRICHLET, 100.0)
+set_boundary("mirrow_right", DIRICHLET, 300.0)
 
 execute()
 
 export_solution("half_circle")
+
+finish_solver()
