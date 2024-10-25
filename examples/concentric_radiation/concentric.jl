57e1f8e26ec7792c9df284f5d50d586c2dedadc7
Leo Basov <leo.basov@dlr.de>
2024-10-25 15:32:21 +0200

    added comment to test case

--- examples/concentric_radiation/concentric.jl
+++ examples/concentric_radiation/concentric.jl
@@ -1,3 +1,5 @@
+# given T0, T1, T2 in this calse T1 should be T1 = 464.9943201 K
+
 using HAMT
 
 read_mesh("examples/concentric_radiation/concentric.msh")
@@ -8,7 +10,7 @@ set_boundary("outer", RADIATION, 1.0, background_radiaton_only=true)
 
 #set_surface("outer", "volumetric_heat_source", 10)
 
-export_mesh("concentric")
+#export_mesh("concentric")
 
 execute()
 
