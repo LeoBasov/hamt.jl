af849e7cf66edef786b7d87e8d64252936d50876
Leo Basov <leo.basov@dlr.de>
2025-12-03 16:09:26 +0100

    updated input files for radiation test case

--- examples/radiation_test_cartesian/radiation_test_cartesian_single.jl
+++ examples/radiation_test_cartesian/radiation_test_cartesian_single.jl
@@ -1,18 +1,16 @@
 using HAMT
 
-read_mesh("examples/radiation_test_cartesian/domain.msh")
+read_mesh("examples/radiation_test_cartesian/domain_single.msh")
 
 set_boundary("buttom", DIRICHLET, 300.0)
-set_boundary("left", DIRICHLET, 300.0)
 
+set_boundary("left", RADIATION, 1.0)
 set_boundary("top", RADIATION, 1.0)
 set_boundary("right", RADIATION, 1.0)
 set_surface("surf", "thermal_conductivity", 100)
 
-set_boundary("circle_right", RADIATION, 1.0)
-set_boundary("circle_left", DIRICHLET, 300.0)
+set_boundary("circle_left", DIRICHLET, 1.0)
 set_surface("circle_left_surf", "thermal_conductivity", 100)
-set_surface("circle_right_surf", "thermal_conductivity", 100)
 
 #set_surface("circle_left_surf", "volumetric_heat_source", 100.0)
 
