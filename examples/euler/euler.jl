17e5a6d5aa494e473160b527211bc10c16f4eddb
Leo Basov <leo.basov@dlr.de>
2025-12-04 17:12:18 +0100

    updated euler case

--- examples/euler/euler.jl
+++ examples/euler/euler.jl
@@ -2,12 +2,12 @@ using HAMT
 
 read_mesh("examples/euler/circle.msh")
 
-set_boundary("top", NEUMANN, 10.0)
+set_boundary("left", NEUMANN, -9.6)
 set_boundary("right", DIRICHLET, 10.0)
 set_boundary("top", NEUMANN, 0.0)
 set_boundary("buttom", NEUMANN, 0.0)
 
-set_boundary("circle", NEUMANN, 0.0)
+set_boundary("cirlce", NEUMANN, 0.0)
 
 execute()
 
